f9aa94d1aa577fb154a4915612b4587d16ffc40a
Anthony Green <green@gmachine.(none)>
2010-01-20 16:22:35 -0500

    Minor init.el tweak

--- init.el
+++ init.el
@@ -309,3 +309,11 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
+
+;; default to better frame titles
+(setq frame-title-format
+      (concat  "%b - emacs@" (system-name)))
+
+;; default to unified diffs
+(setq diff-switches "-u")
+
